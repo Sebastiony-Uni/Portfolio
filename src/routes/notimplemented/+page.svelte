14078693ed5a0fc9e793d6c0832192c34d21cbe7
Sebastiony-Uni <sebastian.westburyprice@students.mq.edu.au>
2023-02-20 17:02:51 +1100

    update bounce back as no projects

---
+++ src/routes/notimplemented/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+
+  import { goto } from "$app/navigation";
+
+</script>
+<div class="flex p-10 flex-col">
+    <p class="text-lg py-5">Thanks for trying to view my projects. Unfortunately this part of the website is not developed yet...</p>
+    <p>Please return to the <a class="text-blue-400 hover:text-purple-400" href="/"> homepage </a> for the time being</p> 
+</div>
